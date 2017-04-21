d8c8305c59a7eff9a5317725a9f0567a1a4c1a83
Con Daly <condaly@Cons-MacBook-Air.local>
2017-04-21 04:09:11 +0100

    Initial Commit

---
+++ NitroCppTest-ConDaly/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NitroCppTest-ConDaly
+//
+//  Created by Con Daly on 21/04/2017.
+//  Copyright © 2017 Con Daly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
